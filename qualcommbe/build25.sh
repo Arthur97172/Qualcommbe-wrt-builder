a550e7dae4e113c1e2dca39f8dc41ee0fb0cdaaa
Arthur97172 <arthur97172@outlook.com>
2026-09-14 12:38:57 +0800

    Remove fitblk from package list in build script

--- qualcommbe/build25.sh
+++ qualcommbe/build25.sh
@@ -27,7 +27,7 @@ PACKAGES=""
 
 # [核心系统]
 PACKAGES="$PACKAGES base-files uci ubus dropbear logd mtd bash htop curl wget ca-bundle ca-certificates"
-PACKAGES="$PACKAGES -dnsmasq dnsmasq-full firewall4 nftables kmod-nft-offload fitblk nano"
+PACKAGES="$PACKAGES -dnsmasq dnsmasq-full firewall4 nftables kmod-nft-offload nano"
 PACKAGES="$PACKAGES ip-full ipset iw ppp ppp-mod-pppoe luci-proto-ppp luci-proto-ipv6"
 PACKAGES="$PACKAGES -odhcpd odhcpd-ipv6only odhcp6c"
 PACKAGES="$PACKAGES -wpad-basic-mbedtls -wpad-mbedtls -wpad-openssl wpad-mesh-openssl"
